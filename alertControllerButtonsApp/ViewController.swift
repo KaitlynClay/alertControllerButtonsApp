f8f921aa809692bafa48548c6dc5c99b0f418e3e
KaitlynClay <122916928+KaitlynClay@users.noreply.github.com>
2024-02-22 18:06:40 -0600

    working multi btns

--- alertControllerButtonsApp/ViewController.swift
+++ alertControllerButtonsApp/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelResult: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        labelResult.numberOfLines = 0
     }
 
 
+    @IBAction func btnTapped(_ sender: UIButton) {
+        let alert = UIAlertController(title: "Warning", message: "The horses are sick!", preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: {action -> Void in
+            self.labelResult.text = "OK"
+        })
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: {action -> Void in
+            self.labelResult.text = "Cancel"
+        })
+        let destroyAction = UIAlertAction(title: "Destroy", style: .destructive, handler: {action -> Void in
+            self.labelResult.text = "Destroy"
+        })
+        alert.addAction(okAction)
+        alert.addAction(cancelAction)
+        alert.addAction(destroyAction)
+        self.present(alert, animated: true, completion: nil)
+    }
 }
 
